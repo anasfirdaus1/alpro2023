65ea90fcd2e421faa91c263ec0744e62589a0e45
anasfirdaus1 <anas.22315041@umkendari.ac.id>
2023-10-17 20:45:23 +0700

    tugas 2 selesai

---
+++ tugas_2/tugas2.py
@@ -0,0 +1,14 @@
+
+if __name__ == "__main__":
+	#tuple
+	x = ("apel", "belimbing", "mangga")
+	#list
+	y = ["apel", "belimbing", "durian"]
+	# dictionary
+	z = {"a": "apple", "b": "belimbing", "c": "leci"}
+	print (type(x))
+	print(x)
+	print (type(y))
+	print(y)
+	print (type(z))
+	print(z)
