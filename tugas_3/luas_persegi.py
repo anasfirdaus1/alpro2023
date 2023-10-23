1e43df05fe397d9b24bd6e34e40d96a82f995d4b
anasfirdaus1 <anas.22315041@umkendari.ac.id>
2023-10-23 21:40:17 +0700

    tugas 3 selesai pak

---
+++ tugas_3/luas_persegi.py
@@ -0,0 +1,6 @@
+# luas persegi
+if __name__ == "__main__":
+	sisi = int(input("masukan angka: "))
+	L = (sisi*sisi)
+	
+	print("luas persegi adalah = ", L, "cm")
---
+++ tugas_3/volume_tabung.py
@@ -0,0 +1,7 @@
+#volume tabung 
+if __name__ == "__main__":
+	d = int(input("masukan diameter: "))
+	t = int(input("masukan tinggi: "))
+	pi = 3.14
+	V = (pi*d*t)
+	print("volume tabung =", V, "cm3")
